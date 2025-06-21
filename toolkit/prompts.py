5f97b0b7832afd0be3ba20028ee14054129f22ff
Vedant K <gamemaker0042@gmail.com>
2025-06-21 12:45:21 +0530

    feat: restructure prompts and add one for function writing

--- toolkit/prompts.py
+++ toolkit/prompts.py
@@ -1,14 +1,29 @@
 """
-This module exports prompts that enable function calling based on the
-model being used. The prompts are stored in markdown files, one per
-model, in the `prompts/` directory.
+This module exports prompts that enable various function calling
+capabilities based on the model being used. The prompts are stored in
+markdown files, one per model, in the `prompts/` directory.
 """
+
+import re
 
 
 def function_calling(model: str) -> str:
     try:
-        with open(f"prompts/{model}.md", encoding="utf-8") as file:
+        with open(f"prompts/function-calling/{model}.md", encoding="utf-8") as file:
             contents = file.read()
             return contents
     except FileNotFoundError:
         print(f"Function calling with {model} is not yet supported.")
+
+
+def function_writing(model: str) -> str:
+    with open("schemas/function.schema.json", encoding="utf-8") as file:
+        schema = file.read()
+
+    try:
+        with open(f"prompts/function-writing/{model}.md", encoding="utf-8") as file:
+            contents = re.sub(r"{schema}", schema, file.read())
+            return contents
+
+    except FileNotFoundError:
+        print(f"Function writing with {model} is not yet supported.")
